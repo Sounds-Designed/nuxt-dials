c9e1d4553ff0194af61bf7e62357c68d7ed5498c
Alex Scott <codetheorist@outlook.com>
2026-01-12 22:12:13 +0000

    feat: improve dials props types

--- src/runtime/schemas/BaseDialPropsSchema.ts
+++ src/runtime/schemas/BaseDialPropsSchema.ts
@@ -2,8 +2,29 @@ import * as z from "zod"
 import type { IBaseDialProps } from "../types";
 
 export const BaseDialPropsSchema = z.object({
-  disabled: z.boolean().optional().default(false),
-  readonly: z.boolean().optional().default(false)
+  disabled: z.boolean().optional(),
+  readonly: z.boolean().optional(),
+  invalid: z.boolean().optional(),
+  fullWidth: z.boolean().optional(),
+  showValue: z.boolean(),
+  name: z.string().optional(),
+  defaultValue: z.union([z.string(), z.number()]).optional(),
+  modelValue: z.union([z.string(), z.number()]).optional(),
+  size: z.number(),
+  step: z.number(),
+  min: z.number(),
+  max: z.number(),
+  tabindex: z.number().optional(),
+  ariaLabel: z.string().optional(),
+  ariaLabelledby: z.string().optional(),
+  rangeColor: z.string(),
+  rangeWidth: z.number(),
+  textColor: z.string(),
+  valueColor: z.string(),
+  valueTemplate: z.union([z.string(), z.function({
+    input: [z.union([z.string(), z.number()])], // parameters (must be an array or a ZodTuple)
+    output: z.string()  // return type
+  })]),
 }) satisfies z.ZodType<IBaseDialProps>;
 
 export default BaseDialPropsSchema;
--- src/runtime/types/props.ts
+++ src/runtime/types/props.ts
@@ -1,9 +1,29 @@
 import * as z from "zod";
 import type { BaseDialPropsSchema, SimpleDialPropsSchema } from "../schemas";
 
+export type ValueTemplateFunction = (value: number | string) => string;
+
 export interface IBaseDialProps {
   disabled?: boolean;
   readonly?: boolean;
+  name?: string;
+  invalid?: boolean;
+  fullWidth?: boolean;
+  showValue?: boolean;
+  defaultValue?: string | number;
+  modelValue?: string | number;
+  size: number;
+  step: number;
+  min: number;
+  max: number;
+  tabindex?: number;
+  ariaLabel?: string;
+  ariaLabelledby?: string;
+  rangeColor?: string;
+  rangeWidth?: number;
+  textColor?: string;
+  valueColor?: string;
+  valueTemplate?: string | ValueTemplateFunction;
 }
 
 export interface ISimpleDialProps extends IBaseDialProps {
